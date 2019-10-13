f3001798ff7be4a314580affde37a867e5e27ce1
Terje Norderhaug <terje@in-progress.com>
2019-10-12 23:27:12 -0700

    Add background

--- src/Landing.js
+++ src/Landing.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import { useBlockstack } from 'react-blockstack'
 
+const templateURL = "https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/njordhov/react-blockstack/tree/master/example"
+
 export default function Landing (props) {
   const { signIn } = useBlockstack()
   return (
-      <main className="jumbotron min-vh-100 mb-0" id="section-1">
+      <main className="jumbotron min-vh-100 mb-0 mt-0" id="section-1">
+        <div className="text-center">
+          <img src="/media/logo.svg" style={{with: "60%", maxWidth: "20em"}}/>
+        </div>
         <h1 className="text-center p-20">React + Blockstack</h1>
         <p className="lead text-center">
-          Use REBL RUN to generate apps based on React and Blockstack.
+          Use the REBL Stack to generate apps based on React and Blockstack.
         </p>
-        <div className="text-center m-10 p-10">
+
+        <div className="text-center">
+          <p>
+            The <a href="https://www.npmjs.com/package/react-blockstack">react-blockstack</a> package
+            for NPM makes it easy to create blockstacks app using modern React hooks.
+          </p>
+
+         <p><a href={templateURL} target="_blank">Download</a> our app template and follow the instructions to get started.</p>
+
+         <p>We have big plans! Keep an eye on this page for further developments.</p>
+      </div>
+
+      {false &&
+      <div className="text-center" style={{marginTop: "3em"}}>
                 <button
                   className="btn btn-primary btn-lg"
                   id="signin-button"
@@ -17,7 +35,7 @@ export default function Landing (props) {
                   onClick={ signIn }>
                   Sign In with Blockstack
                 </button>
-         </div>
+         </div>}
       </main>
   )
 }
--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,6 @@
 import React, {useEffect, useCallback} from 'react'
 import { useBlockstack, useFile } from 'react-blockstack'
 
-const templateURL = "https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/njordhov/react-blockstack/tree/master/example"
-
 export default function Main () {
     const [data, setData] = useFile("config1") //
     useEffect(() => {
@@ -15,16 +13,9 @@ export default function Main () {
       <main className="jumbotron min-vh-100 mb-0">
         <div style={{maxWidth: "60em", margin: "auto"}}>
           <p className="lead">
-          Use REBL RUN to generate apps based on React and Blockstack.
-          </p>
-          <p>
-            The <a href="https://www.npmjs.com/package/react-blockstack">react-blockstack</a> package
-            for NPM makes it easy to create blockstacks app using modern React hooks.
+          Use the REBL Stack to generate apps based on React and Blockstack.
           </p>
 
-         <p><a className="btn btn-primary" href={templateURL} target="_blank">Download</a> our app template and follow the instructions to get started.</p>
-
-         <p>We have big plans! Keep an eye on this page for further developments.</p>
         </div>
       </main>
     )
